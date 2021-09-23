a2e99ad0d5aa2a0e046f9954808dfe9bfea6e858
marcel <mscherzer@gmail.com>
2021-09-23 23:19:54 +1000

    Add unit test for creating discord messages.

---
+++ src/lib/Discord.test.ts
@@ -0,0 +1,109 @@
+import "dotenv/config";
+import { Discord } from "./Discord";
+import {
+  CommitType,
+  LogEntryType,
+  BuildLogEntryType,
+  DeployLogEntryType,
+} from "../types";
+
+const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK || "";
+const DISCORD_AVATAR = process.env.DISCORD_AVATAR || "";
+
+test("create-default-failed-message", async () => {
+  const commit: CommitType = {
+    id: "123456",
+    author: "Jack Sparrow",
+    summary: "Retighten the spiggot on the warp-drive",
+    link: "http://www.github.com",
+  };
+
+  const logEntry: LogEntryType = {
+    id: "123456",
+    type: "unknown",
+    name: "What name",
+    message: "Something has happened",
+  };
+
+  const discord = new Discord();
+  const message = discord.createPipeFailureMessage(
+    "Unit-Test",
+    commit,
+    logEntry
+  );
+
+  // discord.postMessage(message,DISCORD_WEBHOOK,DISCORD_AVATAR);
+  expect(message.description).toBe("Pipeline Failed.");
+});
+
+test("create-build-failed-message", async () => {
+  const commit: CommitType = {
+    id: "123456",
+    author: "Jack Sparrow",
+    summary: "Retighten the spiggot on the warp-drive",
+    link: "http://www.github.com",
+  };
+
+  const logEntry: BuildLogEntryType = {
+    id: "123456",
+    type: "build",
+    name: "What name",
+    message: "Something has happened",
+    build: {
+      logUrl: "https://github.com/quicken/aws-code-pipeline-monitor",
+    },
+  };
+
+  const discord = new Discord();
+  const message = discord.createPipeFailureMessage(
+    "Unit-Test",
+    commit,
+    logEntry
+  );
+
+  // discord.postMessage(message,DISCORD_WEBHOOK,DISCORD_AVATAR);
+  expect(message.fields[0].name).toBe("Commit: 123456");
+  expect(message.fields[1].name).toBe("View Build Log:");
+});
+
+test("create-deploy-failed-message", async () => {
+  const commit: CommitType = {
+    id: "123456",
+    author: "Jack Sparrow",
+    summary: "Retighten the spiggot on the warp-drive",
+    link: "http://www.github.com",
+  };
+
+  const logEntry: DeployLogEntryType = {
+    id: "123456",
+    type: "deploy",
+    name: "What name",
+    summary: "Deployment d-38E5IUD1C failed",
+    deploy: {
+      targets: [
+        { instanceid: "instance-1", diagnostics: null },
+        {
+          instanceid: "instance-2",
+          diagnostics: {
+            errorCode: "",
+            logTail: `. Migration of schema [dbo] may not be reproducible.
+            [stdout]Migrating schema [dbo] to version "2021.09.08.14.55 - Swap Login Creation Link for User Login Access Link in templates"
+            [stderr]Terminated`,
+            message: "",
+            scriptName: "deploy/scripts/flyway_migrate.sh",
+          },
+        },
+      ],
+    },
+  };
+
+  const discord = new Discord();
+  const message = discord.createPipeFailureMessage(
+    "Unit-Test",
+    commit,
+    logEntry
+  );
+
+  // discord.postMessage(message, DISCORD_WEBHOOK, DISCORD_AVATAR);
+  expect(message.fields.length).toBe(3);
+});
