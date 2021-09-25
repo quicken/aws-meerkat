899b126f0da6d7ff14563f9c88ac4873172e5fe7
marcel <mscherzer@gmail.com>
2021-09-25 15:10:07 +1000

    Ensure the build step does not also create a zip file.

--- package.sh
+++ package.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-export NODE_ENV=production
-# rm -rf ./build 
-npx tsc -p tsconfig.json 
-cp ./package.json ./build/
 cd ./build
-yarn install
 zip -r code-pipeline-monitor.zip ./
-echo Build Completed
