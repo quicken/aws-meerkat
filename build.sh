036a17e7625101be32be14d988d6f2f6c99ba45a
marcel <mscherzer@gmail.com>
2021-09-25 19:40:40 +1000

    Ensure bash script fails if any single command fails.

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+set -euo pipefail
+
 export NODE_ENV=production
-# rm -rf ./build 
-npx tsc -p tsconfig.json 
+# rm -rf ./build
+npx tsc -p tsconfig.json
 cp ./package.json ./build/
 cd ./build
 yarn install
