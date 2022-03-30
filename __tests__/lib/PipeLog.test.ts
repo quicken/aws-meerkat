0cd7793b26a29e4647de7cfc98ebf30f5c621765
marcel <mscherzer@gmail.com>
2022-03-30 20:02:25 +1000

    test failed and isfailed methods in PipeLog

--- __tests__/lib/PipeLog.test.ts
+++ __tests__/lib/PipeLog.test.ts
@@ -54,6 +54,60 @@ jest.mock("../../src/lib/GitHub", () => {
 
 const DB_TABLE = process.env.DB_TABLE || "devops-pipeline-monitor";
 
+test("get_failed_gt_one", async () => {
+  const bitBucket = new BitBucket("", "");
+  const pipelog = new PipeLog(DB_TABLE, bitBucket);
+  await pipelog.handlePipelineAction(PIPELINE_SOURCE_ACTION_BITBUCKET);
+  await pipelog.handlePipelineAction(PIPELINE_STAGE_BUILD_ACTION_BUILD_FAILED);
+  await pipelog.handlePipelineAction(
+    PIPELINE_STAGE_DEPLOY_ACTION_DEPLOY_GROUP_RED_FAILED
+  );
+
+  const FIRST_FAILED = pipelog.failed;
+
+  if (FIRST_FAILED) {
+    expect(FIRST_FAILED.id).toBe(
+      "meerkat-testing:56be3e40-853a-4797-9455-f88ce291fdad"
+    );
+    expect(FIRST_FAILED.name).toBe("Build");
+    expect(FIRST_FAILED.type).toBe("build");
+    expect(FIRST_FAILED.summary).toBe(undefined);
+    expect(FIRST_FAILED.link).toBe(
+      "https://console.aws.amazon.com/codebuild/home?region=ap-southeast-2#/builds/meerkat-testing:56be3e40-853a-4797-9455-f88ce291fdad/view/new"
+    );
+  } else {
+    expect(FIRST_FAILED).not.toEqual(null);
+  }
+});
+
+test("get_failed_is_empty", () => {
+  const bitBucket = new BitBucket("", "");
+  const pipelog = new PipeLog(DB_TABLE, bitBucket);
+
+  expect(pipelog.failed).toBe(null);
+});
+
+test("get_is_failed_true", async () => {
+  const bitBucket = new BitBucket("", "");
+  const pipelog = new PipeLog(DB_TABLE, bitBucket);
+  await pipelog.handlePipelineAction(PIPELINE_STAGE_BUILD_ACTION_BUILD_FAILED);
+  expect(pipelog.isFailed).toBe(true);
+});
+
+test("get_is_failed_false", () => {
+  const bitBucket = new BitBucket("", "");
+  const pipelog = new PipeLog(DB_TABLE, bitBucket);
+  expect(pipelog.isFailed).toBe(false);
+});
+
+test("handleCodeStarConnection_BITBUCKET_succeeded", async () => {
+  const bitBucket = new BitBucket("", "");
+  const pipelog = new PipeLog(DB_TABLE, bitBucket);
+  await pipelog.handlePipelineAction(PIPELINE_SOURCE_ACTION_BITBUCKET);
+
+  expect(pipelog.commit.author).toBe("Marcel Scherzet <mscherzer@gmail.com>");
+});
+
 test("handleCodeStarConnection_BITBUCKET_succeeded", async () => {
   const bitBucket = new BitBucket("", "");
   const pipelog = new PipeLog(DB_TABLE, bitBucket);
